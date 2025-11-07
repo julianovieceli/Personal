3bb4d35391e78cce6e18f0c00f2799b260888c72
Juliano Vieceli <julianovieceli@gmail.com>
2025-11-07 17:46:56 -0300

    PhoneValidador

---
+++ Personal.Common.Domain/Validators/PhoneValidador.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Personal.Common.Domain.Validators
+{
+    public class PhoneValidador
+    {
+        public static bool IsValid(string telefone)
+        {
+            // Exemplo de uso da regex para formato brasileiro flexível
+            string padrao = @"^\(?\d{2}\)?\s?(9?\d{4,5})-?(\d{4})$";
+            return Regex.IsMatch(telefone, padrao);
+        }
+    }
+}
