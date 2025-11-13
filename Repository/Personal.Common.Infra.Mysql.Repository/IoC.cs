0835538214bff3fbde4a444e75fb2ea66eeb34c1
Juliano Vieceli <julianovieceli@gmail.com>
2025-11-13 13:18:39 -0300

    AddMySqlDbContext

---
+++ Repository/Personal.Common.Infra.Mysql.Repository/IoC.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Personal.Common.Domain.Interfaces.Repository;
+
+namespace Personal.Common.Infra.Mysql.Repository
+{
+    public static class IoC
+
+    {
+        public static IServiceCollection AddMySqlDbContext(this IServiceCollection services)
+        {
+            return services.AddScoped<IDbContext, MySqlDbcontext>();
+        }
+    }
+}
